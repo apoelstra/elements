c5a2bb8755b96465e994a2ee0c4670dd0cc6013a
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-01-22 23:35:44 +0000

    rust: compute single-jet programs, remove rng from Vec<u8> generator

--- src/txgen/src/generate/mod.rs
+++ src/txgen/src/generate/mod.rs
@@ -176,14 +176,9 @@ impl Generate for Vec<u8> {
         let size = (usize::from(byte1 & 0x03) << 8) + usize::from(byte2);
         assert!(size < 0x400);
 
-        let rng_mul = s.extract_u8()?;
-        let rng_add = s.extract_u8()?;
-        let mut rng_byte = s.extract_u8()?;
-
         let mut data = Vec::with_capacity(size);
         for _ in 0..size {
-            rng_byte = rng_byte.wrapping_mul(rng_mul).wrapping_add(rng_add);
-            data.push(rng_byte);
+            data.push(s.extract_u8()?);
         }
         Some(Sampled { data, size })
     }
